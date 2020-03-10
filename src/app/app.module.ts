4c6c6d92e649164656c6532950a7eccb755af166
VitaliiLynnyk <linnik2101@gmail.com>
2020-03-10 14:51:27 +0100

    add empty containers / simple structure with modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { ParticleDirective } from './particle.directive';
+import { ParticleDirective } from './directives/particle.directive';
 
 import { AppComponent } from './app.component';
 
+import { HomeModule } from './containers/home/home.module';
+import { FooterModule } from './containers/footer/footer.module';
+import { HeaderModule } from './containers/header/header.module';
 
 @NgModule({
-  declarations: [ AppComponent, ParticleDirective ],
-  imports: [ BrowserModule ],
+  declarations: [
+    AppComponent,
+    ParticleDirective
+  ],
+  imports: [
+    HomeModule,
+    HeaderModule,
+    FooterModule,
+    BrowserModule
+  ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
