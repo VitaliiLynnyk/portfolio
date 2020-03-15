8d44da5f3218ee5aa04dda193cefe52ade31cc1f
VitaliiLynnyk <linnik2101@gmail.com>
2020-03-15 19:18:11 +0100

    add projects test page with cards styling

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -5,4 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './projects.html',
   styleUrls: [ './projects.scss' ]
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  arrOfProjects = [
+    {
+      imgUrl: 'https://user-images.githubusercontent.com/22242511/73015382-3193d600-3e1c-11ea-84db-c997627864ff.png',
+      name: 'TestSystem',
+      projectUrl: 'https://github.com/VitaliiLynnyk/web-app-practice',
+      stack: [
+        'Angular',
+        'Node',
+        'PostgreSql',
+        'SCSS'
+      ]
+    },
+    {
+      imgUrl: 'https://user-images.githubusercontent.com/22242511/76707580-9b2fb280-66f0-11ea-830f-20c37881a38c.png',
+      name: 'Favorite Fonts',
+      projectUrl: 'https://github.com/VitaliiLynnyk/soloproject-tier2-favfonts',
+      stack: [
+        'Angular',
+        'HTML',
+        'SCSS'
+      ]
+    }
+  ];
+}
